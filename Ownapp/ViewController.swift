2ea8ca4001edf3556bfba5847dcac2d38e704dc8
vlad <vladporaiko@gmail.com>
2019-01-29 16:13:56 +0200

    Initial Commit

---
+++ Ownapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ownapp
+//
+//  Created by Владислав on 1/29/19.
+//  Copyright © 2019 vladporaiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
